c0d00b598cc98c512356c86a3ccdf89d7f5a2162
Igor Pershin <igor.pershin.me@gmail.com>
2023-02-28 23:04:43 +0300

    formatting fixes

--- questions/1.ts
+++ questions/1.ts
@@ -1,11 +1,11 @@
 // Как задать типы свойств внутри объекта?
 
 interface User {
-  name: string;
-  id: number;
+    name: string;
+    id: number;
 }
 
 const user: User = {
-  name: "Hayes",
-  id: 0,
+    name: "Hayes",
+    id: 0,
 };
